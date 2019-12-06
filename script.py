b2c19e96e2283e706832d0d8c4ce6eaec440a1c7
Christopher Lamm <chris@theantichris.com>
2019-12-06 07:49:59 -0500

    added hash function

--- script.py
+++ script.py
@@ -2,3 +2,8 @@ class HashMap:
   def __init__(self, array_size):
     self.array_size = array_size
     self.array = [None for i in range(self.array_size)]
+
+  def hash(self, key):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code
