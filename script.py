7e3826fb7921ee1b6486cee1d2fb2a4172350562
Christopher Lamm <chris@theantichris.com>
2019-12-06 07:57:42 -0500

    added assign function

--- script.py
+++ script.py
@@ -3,6 +3,11 @@ class HashMap:
     self.array_size = array_size
     self.array = [None for i in range(self.array_size)]
 
+  def assign(self, key, value):
+      hash_code = self.hash(key)
+      index = self.compress(hash_code)
+      self.array[index] = value
+
   def hash(self, key):
     key_bytes = key.encode() # Convert key into list of bytes
     hash_code = sum(key_bytes) # Add the bytes together
